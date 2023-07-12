65e8259e8ec8f63b45bd62fc3f5d1e99939e0c0c
Marcel Berlin <berlin_m@gmx.de>
2023-07-12 21:16:09 +0200

    sidenavMenu basics

--- src/app/dashboard/menu-channels-workspaces/menu-sidenav/menu-sidenav.component.ts
+++ src/app/dashboard/menu-channels-workspaces/menu-sidenav/menu-sidenav.component.ts
@@ -1,18 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu-sidenav',
   templateUrl: './menu-sidenav.component.html',
-  styleUrls: ['./menu-sidenav.component.scss']
-})
-export class MenuSidenavComponent {
+  styleUrls: ['./menu-sidenav.component.scss'],  
 
-  imagePath: string = 'assets/img/sidenav/newMessage.png';
+})
+
+export class MenuSidenavComponent implements OnInit {
+
+  newMessagePath: string = 'assets/img/sidenav/newMessage.png';
+  channelsPath: string = 'assets/img/sidenav/channel_closed.png';
+  addPathChannel: string = 'assets/img/sidenav/add.png';
+  addPathMessage: string = 'assets/img/sidenav/add.png';
+  firstTagPath: string = 'assets/img/sidenav/tag.png';
+  secondTagPath: string = 'assets/img/sidenav/tag.png';
+  thirdTagPath: string = 'assets/img/sidenav/tag.png';
+  directMessagePath: string = 'assets/img/sidenav/direct_message_closed.png';
+
+
 
 
   constructor() {}
 
- 
+  ngOnInit(): void {
+    
+  }
 
+ 
+ 
 
 }
