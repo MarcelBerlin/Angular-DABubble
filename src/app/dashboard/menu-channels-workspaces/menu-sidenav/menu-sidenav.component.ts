090f6113bc07502197981fa15e0cb0aeb8e3268a
Marcel Berlin <berlin_m@gmx.de>
2023-07-13 18:36:07 +0200

    added toggle animation channels section

--- src/app/dashboard/menu-channels-workspaces/menu-sidenav/menu-sidenav.component.ts
+++ src/app/dashboard/menu-channels-workspaces/menu-sidenav/menu-sidenav.component.ts
@@ -1,13 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
+
+interface Tag {
+  name: string;
+  imagePath: string;
+}
 
 @Component({
   selector: 'app-menu-sidenav',
   templateUrl: './menu-sidenav.component.html',
   styleUrls: ['./menu-sidenav.component.scss'],  
+  animations: [
+    trigger('tagAnimation', [
+      state('visible', style({ opacity: 1 })),
+      state('hidden', style({ opacity: 0, display: 'none' })),
+      transition('visible <=> hidden', animate('175ms ease-in-out'))
+    ])
+  ]
 
 })
 
 export class MenuSidenavComponent implements OnInit {
+
+  tagState = 'visible';
 
   newMessagePath: string = 'assets/img/sidenav/newMessage.png';
   channelsPath: string = 'assets/img/sidenav/channel_closed.png';
@@ -18,8 +34,19 @@ export class MenuSidenavComponent implements OnInit {
   thirdTagPath: string = 'assets/img/sidenav/tag.png';
   directMessagePath: string = 'assets/img/sidenav/direct_message_closed.png';
 
+  channelsVisible: boolean = true;
+  directMessageUserVisible: boolean = true;
 
+  tags: Tag[] = [
+    { name: 'Allgemein', imagePath: 'assets/img/sidenav/tag.png' },
+    { name: 'Entwicklerteam', imagePath: 'assets/img/sidenav/tag.png' },
+    // ... andere Tags ...
+  ];
 
+  addTag(tag: string) {
+    const newTag: Tag = { name: tag, imagePath: 'assets/img/sidenav/tag.png' }
+    this.tags.push(newTag); // Hinzufügen des neuen Tags zum Array
+  }
 
   constructor() {}
 
@@ -27,7 +54,13 @@ export class MenuSidenavComponent implements OnInit {
     
   }
 
- 
+  toggleChannels() {
+    this.channelsVisible = !this.channelsVisible;
+  }
+
+  toggleDirectMessage() {
+    this.directMessageUserVisible = !this.directMessageUserVisible;
+  }
  
 
 }
