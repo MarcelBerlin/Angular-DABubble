9eb127ae8c4ea99432c1e7c8f854578a734ab563
HassSebastian <sebastian.hass76@gmail.com>
2023-07-16 12:01:48 +0200

    show and hide nav toggle img added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,14 +4,32 @@ import { DashboardComponentsShowHideService } from './dashboard-components-show-
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
-  
+  imageUrl: string = 'assets/img/nav-hide.png';
 
-  constructor(public dcshService: DashboardComponentsShowHideService) { }
-  
+  constructor(public dcshService: DashboardComponentsShowHideService) {}
+
   hideShowNavigation() {
     this.dcshService.hideShowNavigation();
+  }
+
+  changeImageOnHover(isHover: boolean) {
+    this.dcshService.hideNavigation
+      ? this.showNavImg(isHover)
+      : this.hideNavImg(isHover);
+  }
+
+  showNavImg(isHover: boolean) {
+    isHover
+      ? (this.imageUrl = 'assets/img/nav-show-hover.png')
+      : (this.imageUrl = 'assets/img/nav-show.png');
+  }
+
+  hideNavImg(isHover: boolean) {
+    isHover
+      ? (this.imageUrl = 'assets/img/nav-hide-hover.png')
+      : (this.imageUrl = 'assets/img/nav-hide.png');
   }
 }
