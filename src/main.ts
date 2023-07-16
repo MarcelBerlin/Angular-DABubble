742b25bbdfda91c3f54ce41e2ea5af16899945aa
Stefan Boskamp <stboskamp@gmail.com>
2023-07-16 20:44:42 +0200

    main ts add

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,9 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+  window.addEventListener('beforeunload', function(event) {
+    // Benachrichtigung anzeigen
+    event.preventDefault();
+    event.returnValue = 'Leave';
+  });
