3f16de278401b837873e8fda1680aad2e77f5f87
Stefan Boskamp <stboskamp@gmail.com>
2023-07-16 19:06:12 +0200

    guest Login created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,32 +3,28 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-<<<<<<< HEAD
 import { CreateAccountComponent } from './create-account/create-account.component';
-=======
-import { ChannelFocusComponent } from './dashboard/main-chat/channel-focus/channel-focus.component';
->>>>>>> 85f3972591479bef30cf52cba909f5ef221c90f6
 
 const routes: Routes = [
   // {path: '', component: },
   // {path: '', component: },
-  { 
-    path: 'forgot_password', 
+  {
+    path: 'forgot_password',
     component: ForgotPasswordComponent
   },
   {
     path: 'dashboard',
     component: DashboardComponent
   },
-  {path: 'createAccount', 
-  component: CreateAccountComponent
+  {
+    path: 'createAccount',
+    component: CreateAccountComponent
   },
   {
     path: '**',
     component: LoginComponent,
     /* dieser Path muss zwingend am ende des routings stehen */
   },
-  
 ];
 
 @NgModule({
