51f0cd1cdf7bc7c22b42d942d15a7d68233e83b9
unknown <sebastian.hass76@gmail.com>
2023-07-19 15:21:47 +0200

    main-chat messagefield placeholder edit

--- src/app/dashboard/main-chat/main-chat-messagefield/main-chat-messagefield.component.ts
+++ src/app/dashboard/main-chat/main-chat-messagefield/main-chat-messagefield.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+import { VariablesService } from 'src/app/services/variables.service';
+import { DashboardComponentsShowHideService } from '../../dashboard-components-show-hide.service';
 
 @Component({
   selector: 'app-main-chat-messagefield',
   templateUrl: './main-chat-messagefield.component.html',
-  styleUrls: ['./main-chat-messagefield.component.scss']
+  styleUrls: ['./main-chat-messagefield.component.scss'],
 })
 export class MainChatMessagefieldComponent {
+  constructor(
+    public varService: VariablesService,
+    public dataService: DataService,
+  ) { }
+  
+  currentUser() {
+    return (
+      this.dataService.loggedInUserData.email ===
+      this.dataService.userData[this.varService.selectedUserToMessage].email
+    );
+  }
 
 }
