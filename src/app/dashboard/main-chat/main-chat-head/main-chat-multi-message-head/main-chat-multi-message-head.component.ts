a107c0de8ecceb9066c663fb716648d100a30259
HassSebastian <sebastian.hass76@gmail.com>
2023-07-21 10:25:44 +0200

    implement search in main-chat

--- src/app/dashboard/main-chat/main-chat-head/main-chat-multi-message-head/main-chat-multi-message-head.component.ts
+++ src/app/dashboard/main-chat/main-chat-head/main-chat-multi-message-head/main-chat-multi-message-head.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
+import { DialogAddService } from 'src/app/services/dialog-add.service';
 
 @Component({
   selector: 'app-main-chat-multi-message-head',
   templateUrl: './main-chat-multi-message-head.component.html',
-  styleUrls: ['./main-chat-multi-message-head.component.scss']
+  styleUrls: ['./main-chat-multi-message-head.component.scss'],
 })
 export class MainChatMultiMessageHeadComponent {
-  constructor(public dataService: DataService) { }
+  inputValue: string = '';
+  selectedArray: any[];
+  setItem: string = '';
+  sign: string = '';
+
+  constructor(
+    public dataService: DataService,
+    private dialogAddService: DialogAddService
+  ) {}
+
+  inputAsValue() {
+    if (!this.inputValue) {
+      this.selectedArray = null;
+      return null;
+    }
+    this.setItem = this.inputValue.charAt(0) === '@' ? 'email' : 'name';
+    this.selectedArray = this.inputValue.charAt(0) === '#'
+      ? this.dialogAddService.tagsData
+      : this.dataService.userData;
+    this.sign = this.inputValue.charAt(0);
+    return this.selectedArray;
+  }
+
+  indexOfSelection(index: number) {
+    console.log('array',this.selectedArray);
+    console.log('Index:', index);
+    console.log('setItem',this.setItem);
+    
+    console.log('zeichen:', this.sign);
+    console.log(this.sign,this.selectedArray[index][this.setItem]);
+    
+    
+  }
 }
