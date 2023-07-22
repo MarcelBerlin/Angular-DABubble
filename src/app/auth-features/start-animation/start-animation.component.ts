f397f353e949a435f8adba5890a23fc2bbb9a8ae
Stefan Boskamp <stboskamp@gmail.com>
2023-07-22 20:05:16 +0200

    Login responsive started

--- src/app/auth-features/start-animation/start-animation.component.ts
+++ src/app/auth-features/start-animation/start-animation.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-start-animation',
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./start-animation.component.scss']
 })
 export class StartAnimationComponent {
+  windowWidth: number;
 
+
+  constructor() {
+    this.windowWidth = window.innerWidth;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.windowWidth = (event.target as Window).innerWidth;
+  }
+
+  isWindowWidthLessThan650(): boolean {
+    return this.windowWidth < 650;
+  }
 }
