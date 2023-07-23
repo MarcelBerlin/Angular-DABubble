8b71c6425b9920bfb364762c2bdf6c156e8f48f1
Stefan Boskamp <stboskamp@gmail.com>
2023-07-23 08:29:36 +0200

    mobile animation finished

--- src/app/auth-features/start-animation/start-animation.component.ts
+++ src/app/auth-features/start-animation/start-animation.component.ts
@@ -10,14 +10,26 @@ export class StartAnimationComponent {
 
 
   constructor() {
-    this.windowWidth = window.innerWidth;
+    this.windowWidth = window.innerWidth; // Initialize windowWidth with the current window width
   }
 
+
+  /**
+   * Listens for the 'resize' event of the window and updates the window width.
+   * 
+   *  @param event - obje The eventct of the 'resize' event. 
+   */
   @HostListener('window:resize', ['$event'])
   onResize(event: Event) {
     this.windowWidth = (event.target as Window).innerWidth;
   }
 
+
+  /**
+   * Checks if the window width is less than 650 pixels.
+   * 
+   * @returns {boolean} - `true` if the window width is less than 650 pixels, otherwise `false`. 
+   */
   isWindowWidthLessThan650(): boolean {
     return this.windowWidth < 650;
   }
