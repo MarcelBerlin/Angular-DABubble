e6915438d5f8ce30f04fcb48c709e37532b49c2b
HassSebastian <sebastian.hass76@gmail.com>
2023-07-28 12:24:42 +0200

    update search methode

--- src/app/dashboard/main-chat/main-chat-head/main-chat-multi-message-head/main-chat-multi-message-head.component.ts
+++ src/app/dashboard/main-chat/main-chat-head/main-chat-multi-message-head/main-chat-multi-message-head.component.ts
@@ -1,56 +1,3 @@
-// import { Component, ElementRef, ViewChild } from '@angular/core';
-// import { FormControl } from '@angular/forms';
-// import { Observable, map, startWith } from 'rxjs';
-// import { DataService } from 'src/app/services/data.service';
-// import { DialogAddService } from 'src/app/services/dialog-add.service';
-// import { VariablesService } from 'src/app/services/variables.service';
-
-// @Component({
-//   selector: 'app-main-chat-multi-message-head',
-//   templateUrl: './main-chat-multi-message-head.component.html',
-//   styleUrls: ['./main-chat-multi-message-head.component.scss'],
-// })
-// export class MainChatMultiMessageHeadComponent {
-//   inputValue: string = '';
-//   selectedArray: any[] = ['dataService.dataUser', 'dialogAddService.tags'];
-//   property: string = '';
-//   sign: string = '';
-
-//   constructor(
-//     public dataService: DataService,
-//     private dialogAddService: DialogAddService,
-//     private varService: VariablesService
-//   ) {}
-
-//   control = new FormControl('');
-//   filteredInputValue: Observable<string[]>;
-
-//   ngOnInit() {
-//     this.filteredInputValue = this.control.valueChanges.pipe(
-//       startWith(''),
-//       map((value) => this._filter(value || ''))
-//     );
-//   }
-
-//   private _filter(value: string): string[] {
-//     const filterValue = this._normalizeValue(value);
-//     const filterData: string[] = [];
-//     for (let i = 0; i < this.dataService.userData.length; i++) {
-//       const filteredItem = this.dataService.userData[i].name.filter((item) =>
-//         this._normalizeValue(item).includes(filterValue)
-//       );
-//       filterData.push(...filteredItem);
-//     }
-//     // return this.dataService.userData.filter((item) =>
-//     //   this._normalizeValue(item).includes(filterValue)
-//     // );
-//     return filterData;
-//   }
-
-//   private _normalizeValue(value: string): string {
-//     return value.toLowerCase().replace(/\s/g, '');
-//   }
-// }
 
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
@@ -67,12 +14,6 @@ import { VariablesService } from 'src/app/services/variables.service';
 })
 export class MainChatMultiMessageHeadComponent {
   control = new FormControl('');
-  streets: string[] = [
-    'Champs-Élysées',
-    'Lombard Street',
-    'Abbey Road',
-    'Fifth Avenue',
-  ];
   filteredArrays: Observable<string[]>;
   sign: string = '';
 
