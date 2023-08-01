270d09a934ed2b8706df482851bbfa55f20da96c
Felix Röder <xfeli537@gmail.com>
2023-08-01 12:56:07 +0200

    reaction bar

--- src/app/dashboard/secondary-chat/secondary-chat-messagefield/secondary-chat-messagefield.component.ts
+++ src/app/dashboard/secondary-chat/secondary-chat-messagefield/secondary-chat-messagefield.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
 import { MessageService } from 'src/app/services/messages.service';
-import { DatePipe } from '@angular/common';
+import { DatePipe, getLocaleExtraDayPeriodRules } from '@angular/common';
 
 @Component({
   selector: 'app-secondary-chat-messagefield',
@@ -15,24 +15,43 @@ export class SecondaryChatMessagefieldComponent {
   public isEmojiPickerVisible: boolean;
   public content: any = 'MUSTERNACHRICHT';
   public actualTime: any = this.time.transform((new Date), 'HH:mm:ss');
-  public sentTime: string = this.actualTime // change later to sending message time
+  public sentTime: any = 13 + '.' + 12 + 'Uhr' // change later to sending message time
   public message: any = {};
   public newMessages: number = 0;
+  public newReaction: any = [];
+  public reaction: boolean = false;
+  public reactionNumber: any = 0;
+
 
   constructor(public getUser: DataService, public getMessage: MessageService, public time: DatePipe) {
-    setTimeout(() => this.getMessages(), 500);
+    setTimeout(() => this.getMessages(), 800);
   }
+
 
   public addEmoji(event) {
-    if (this.emoji) {
-      this.emoji = `${this.emoji}${event.emoji.native}`;
-      this.isEmojiPickerVisible = false;
-    }
+    this.reaction = true;
+    this.newReaction += `${this.emoji}${event.emoji.native}`;
+    this.isEmojiPickerVisible = false;
+    this.reactionNumber ++;
+    // Emoji dem User zuordnen.
+    // if (this.reactionNumber > 1) {
+    //   this.deleteDoubledEmojis(this.newReaction);
+    // }
   }
 
+  // deleteDoubledEmojis(doubledEmoji) {
+  //   for (let i = 0; i < doubledEmoji.length; i++) {
+  //     if (doubledEmoji[i].emoji && doubledEmoji.filter(item => item.emoji === doubledEmoji[i].emoji).length > 1) {
+  //       doubledEmoji.splice(i, 1);
+  //       i--;
+  //     }
+  //   }
+  // }
+
+
   async getMessages() {
-      // *ngFor all messages from channel
-    this.message = 
+    // *ngFor all messages from channel
+    this.message =
     {
       name: this.getUser.loggedInUserData.name, // user from channel
       time: this.sentTime,                      // time from channel
@@ -41,6 +60,6 @@ export class SecondaryChatMessagefieldComponent {
       emoji: this.emoji                         // reaction bar from channel
     };
 
-    console.log('thread-mesage',this.message);
+    console.log('thread-mesage', this.message);
   };
 }
