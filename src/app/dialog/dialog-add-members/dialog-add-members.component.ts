6e3ac48f85d417cbd320db5a9d005ccca08d2c38
HassSebastian <sebastian.hass76@gmail.com>
2023-08-03 20:18:15 +0200

    add-members and channel-edit variablen edit

--- src/app/dialog/dialog-add-members/dialog-add-members.component.ts
+++ src/app/dialog/dialog-add-members/dialog-add-members.component.ts
@@ -1,5 +1,10 @@
 import { DialogRef } from '@angular/cdk/dialog';
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable, map, startWith } from 'rxjs';
+import { DataService } from 'src/app/services/data.service';
+import { DialogAddService } from 'src/app/services/dialog-add.service';
+import { VariablesService } from 'src/app/services/variables.service';
 
 @Component({
   selector: 'app-dialog-add-members',
@@ -7,7 +12,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./dialog-add-members.component.scss'],
 })
 export class DialogAddMembersComponent {
-  constructor(private dialogRef: DialogRef) {}
+  control = new FormControl('');
+  filteredArrays: Observable<string[]>;
+  property: string = 'name';
+
+  constructor(
+    private dialogRef: DialogRef,
+    public tagChannel: DialogAddService,
+    public variableService: VariablesService,
+    public dataService: DataService
+  ) {}
+
+  ngOnInit() {
+    this.filteredArrays = this.control.valueChanges.pipe(
+      map((value) => this._filter(value || ''))
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = this._normalizeValue(value);
+    return this.dataService.userData.filter((element) =>
+      this._normalizeValue(element.name).includes(filterValue)
+    );
+  }
+
+  private _normalizeValue(value: string): string {
+    return value.toLowerCase().replace(/\s/g, '');
+  }
 
   /**
    * Closes the current dialog.
