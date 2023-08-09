5663a685b90efa6ef2abe6ccb271cdae855287ee
Felix Röder <xfeli537@gmail.com>
2023-08-09 16:04:10 +0200

    emoji-function

--- src/app/dashboard/secondary-chat/secondary-chat-messagefield/secondary-chat-messagefield.component.ts
+++ src/app/dashboard/secondary-chat/secondary-chat-messagefield/secondary-chat-messagefield.component.ts
@@ -5,7 +5,6 @@ import { ChatService } from 'src/app/services/chat.service';
 import { AppComponent } from 'src/app/app.component';
 import { VariablesService } from 'src/app/services/variables.service';
 import { DialogAddService } from 'src/app/services/dialog-add.service';
-import {MatTooltip, MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-secondary-chat-messagefield',
@@ -15,7 +14,7 @@ import {MatTooltip, MatTooltipModule} from '@angular/material/tooltip';
 
 export class SecondaryChatMessagefieldComponent {
 
-  @ViewChild('threadReaction') threadReaction: any;
+  @ViewChild('reactions') reactions: ElementRef;
 
   public content: any = '';       // wieder loeschen, wenn main chat fertig ist
   public sentTime: any = '';      // wieder loeschen, wenn main chat fertig ist
@@ -24,36 +23,84 @@ export class SecondaryChatMessagefieldComponent {
   public userImg: string = '';    // wieder loeschen, wenn main chat fertig ist
   public allMessages: any = [];   // wieder loeschen, wenn main chat fertig ist
 
-  public threadEmoji: boolean = false;
+  public threadEmojiLeft: boolean = false;
+  public threadEmojiRight: boolean = false;
+
   public emojiPickerRight: boolean = false;
   public emojiPickerLeft: boolean = false;
   public emoji: string = '';
-  public reactionArr: any = [];
+  public reactionArrLeft: any = [];
+  public reactionArrRight: any = [];
   public emojiCounter: number = 0;
-
-
 
   constructor(public getUser: DataService,
     public app: AppComponent,
     public addService: DialogAddService,
     public varService: VariablesService,
     public getMessage: MessageService,
-    public chatService: ChatService) 
-    {
-      console.log(this.addService.tagsData);
+    public chatService: ChatService) {
+  }
+
+
+  // WIRD SPÄTER ALLES NOCH GEKÜRZT! 
+  public addEmojiLeft(event) {
+    this.threadEmojiLeft = true;
+    this.reactionArrLeft = `${this.emoji}${event.emoji.native}`;
+    this.getMessage.emojis.push(this.reactionArrLeft); // speichern in firebase?
+    this.emojiPickerLeft = false;
+
+    if (this.getMessage.emojis.length > 1) {
+      this.emojiFilterLeft(this.getMessage.emojis);
     }
+  }
 
-    public addEmoji(event) {
-      this.threadEmoji = true;
-      this.reactionArr += `${this.emoji }${event.emoji.native}`;
-      // this.emojiCounter++; bei selben emoji = anzahl dahinter
-      this.getMessage.emojis.push(this.reactionArr);
-      if(this.emojiPickerLeft) { this.emojiPickerLeft = false}
-      if(this.emojiPickerRight) { this.emojiPickerRight = false}
+  emojiFilterLeft(reactionArr) {
+    const emojiCountMapLeft = new Map();
+    let reactionBarLeft = document.getElementById("reactionsLeft"); // span für linke seite, wo emojis angezeigt werden
 
+    reactionArr.forEach(emoji => {
+      if (emojiCountMapLeft.has(emoji)) {
+        emojiCountMapLeft.set(emoji, emojiCountMapLeft.get(emoji) + 1);
+      } else { emojiCountMapLeft.set(emoji, 1); }
+    });
+
+    emojiCountMapLeft.forEach((count, emoji) => {
+      reactionBarLeft.innerHTML = '';
+      reactionBarLeft.innerHTML += `${emoji} ${count > 1 ? count : ""}`;
+      this.emojiPickerLeft = false;
+    });
+  }
+
+
+  public addEmojiRight(event) {
+    this.threadEmojiRight = true;
+    this.reactionArrRight = `${this.emoji}${event.emoji.native}`;
+    this.getMessage.emojis.push(this.reactionArrRight); // speichern in firebase?
+    this.emojiPickerRight = false;
+
+    if (this.getMessage.emojis.length > 1) {
+      this.emojiFilterRight(this.getMessage.emojis);
     }
+  }
 
-  
+
+  emojiFilterRight(reactionArr) {
+    const emojiCountMapRight = new Map();
+    let reactionBarRight = document.getElementById("reactionsRight"); // span für rechte seite, wo emojis angezeigt werden
+
+    reactionArr.forEach(emoji => {
+      if (emojiCountMapRight.has(emoji)) {
+        emojiCountMapRight.set(emoji, emojiCountMapRight.get(emoji) + 1);
+      } else { emojiCountMapRight.set(emoji, 1); }
+    });
+
+    emojiCountMapRight.forEach((count, emoji) => {
+      this.emojiPickerRight = true;
+      reactionBarRight.innerHTML = '';
+      reactionBarRight.innerHTML += `${emoji} ${count > 1 ? count : ""}`;
+      this.emojiPickerRight = false;
+    });
+  }
 
 
   /**
