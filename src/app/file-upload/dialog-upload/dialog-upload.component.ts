bba099bbaed98bf843032cf666fdf3374034e3a6
Stefan Boskamp <stboskamp@gmail.com>
2023-08-21 15:51:38 +0200

    profil img update test

--- src/app/file-upload/dialog-upload/dialog-upload.component.ts
+++ src/app/file-upload/dialog-upload/dialog-upload.component.ts
@@ -31,33 +31,47 @@ export class DialogUploadComponent {
   async log() {
     await this.showUrl();
     // await this.saveUser();
+    this.dataService.updateUser();
     this.dialogRef.close();
   }
 
-  async saveUser() {
-    // this.loading = true;
+  // async saveUser() {
+  //   // this.loading = true;
 
-    const qData = doc(this.firestore, 'users', this.dataService.loggedInUserData.id);
-    const newData = this.user.toJSON();
-    // const newData = { img: this.chatDataSet.id, };
-    updateDoc(qData, newData).then(() => {
-      console.log('Update done');
-      this.loading = false;
-      this.dialogRef.close();
-    }).catch((error) => {
-      console.log(error, 'update failed');
-      this.loading = false;
-    })
-  }
+  //   const qData = doc(this.firestore, 'users', this.dataService.loggedInUserData.id);
+  //   console.log(this.user);
+  //   const newData = this.user.toJSON();
+  //   // const newData = { img: this.chatDataSet.id, };
+  //   updateDoc(qData, newData).then(() => {
+  //     console.log('Update done');
+  //     this.loading = false;
+  //     this.dialogRef.close();
+  //   }).catch((error) => {
+  //     console.log(error, 'update failed');
+  //     this.loading = false;
+  //   })
+  // }
 
   async showUrl() {
     let urlA: any = document.getElementById('url')?.innerHTML;
     if (urlA?.length < 5) {
       setTimeout(this.showUrl, 1000);
     } else {
-      // this.user.profilImageUrl = this.fileService.filePath;
       this.dataService.loggedInUserData.img = this.fileService.lastUpload;
+      // this.setNewUserData();
     }
   }
+
+
+  // setNewUserData():void {
+  //   this.user.name = this.dataService.loggedInUserData.name;
+  //   this.user.img = this.dataService.loggedInUserData.img;
+  //   this.user.email = this.dataService.loggedInUserData.email;
+  //   this.user.online = this.dataService.loggedInUserData.online;
+  //   this.user.directChats = this.dataService.loggedInUserData.directChats;
+  //   // if (this.dataService.loggedInUserData.channels){
+  //   //   this.user.channels = this.dataService.loggedInUserData.channels;
+  //   // }
+  // }
 
 }
