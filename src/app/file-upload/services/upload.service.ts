41dc3ae55f0bb21164c981ce9254b9d9a831c1af
Stefan Boskamp <stboskamp@gmail.com>
2023-08-26 13:29:41 +0200

    upload.service committed

--- src/app/file-upload/services/upload.service.ts
+++ src/app/file-upload/services/upload.service.ts
@@ -1,9 +1,6 @@
-import { Injectable } from '@angular/core';
-import { Component } from '@angular/core';
+import { Injectable, Component } from '@angular/core';
 import { FileUploadService } from './file-upload.service';
 import { FileUpload } from '../models/file-upload.model';
-import { JsonPipe } from '@angular/common';
-
 @Injectable({
   providedIn: 'root'
 })
@@ -13,55 +10,58 @@ export class UploadService {
   percentage = 0;
   fileLink: string = '';
 
+
   constructor(private uploadService: FileUploadService) { }
 
+
+  /**
+   * Handles the selection of files for uploading.
+   * 
+   * @param {Event} event - The event object containing selected files.
+   * @returns {void}
+   */
   selectFile(event: any): void {
     this.selectedFiles = undefined;
     this.selectedFiles = event.target.files;
-    if (this.selectedFiles.length > 0){
-      console.log('Selected File: ', this.selectedFiles);
-      this.upload();
-    } else {
-      console.log('No Selected File: ', this.selectedFiles);
-      this.uploadService.profileImgUpload = false;
-    }
-   
+    this.upload();
   }
 
 
-
+  /**
+   * Initiates the file upload process for the selected file.
+   * If a file is selected, it invokes the 'uploadFile()' function.
+   * 
+   * @returns {void}
+   */
   upload(): void {
     if (this.selectedFiles) {
       const file: File | null = this.selectedFiles.item(0);
       this.selectedFiles = undefined;
-
       if (file) {
-        this.currentFileUpload = new FileUpload(file);
+        this.uploadFile(file);
+      }
+    }
+  }
+
+
+/**
+ * Uploads the specified file using the 'uploadService'.
+ * Updates the 'currentFileUpload' and 'percentage' properties during upload.
+ * Handles progress updates and errors through subscriptions.
+ * 
+ * @param {File} file - The file to be uploaded.
+ * @returns {void}
+ */
+  uploadFile(file: File): void {
+    this.currentFileUpload = new FileUpload(file);
         this.uploadService.pushFileToStorage(this.currentFileUpload).subscribe(
-          
           percentage => {
             this.percentage = Math.round(percentage ? percentage : 0);
-            console.log(this.percentage);
           },
           error => {
             console.log(error);
           }
         );
-      }
-    }
   }
-
-  // showUrl(){
-  //   let urlA: any = document.getElementById('url')?.innerHTML;
-  //   if (urlA?.length < 5) {
-  //     setTimeout(this.showUrl, 1000);
-  //   }else{
-  //     console.log('link', urlA);
-      
-  //   }
-    
-  // }
-
-
 }
 
