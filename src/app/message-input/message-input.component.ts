f8b4062044f0b4768e1a3bf964025d6eaeada9e1
Stefan Boskamp <stboskamp@gmail.com>
2023-09-27 12:00:55 +0200

    new message input clean code work

--- src/app/message-input/message-input.component.ts
+++ src/app/message-input/message-input.component.ts
@@ -26,68 +26,121 @@ export class MessageInputComponent {
     public dataService: DataService,
     private fileUploadService: FileUploadService,
     private directChatService: DirectChatService
-    ) { }
+  ) { }
 
 
-    /**
-     * Angular lifecycle hook that is called after the component has been initialized.
-     * Subscribes to the myVariable$ observable from inputService, and when a new value is emitted,
-     * it triggers the startApplicableButtonAction function if the new value is truthy.
-     *
-     * @returns {void}
-     */
-    ngOnInit():void {
-      this.inputService.myVariable$.subscribe((newValue) => {
-        if (newValue)  this.startApplicableButtonAction();
-      });
+  ngAfterViewInit() {
+    this.restorePlaceholder();
+  }
+
+
+  /**
+   * Removes the placeholder element if it exists.
+   *
+   * @returns {void}
+   */
+  removePlaceholder(): void {
+    if (document.querySelector('.placeholder')) {
+      const editableDiv = this.editableDiv.nativeElement;
+      const placeholderElement = editableDiv.querySelector('.placeholder');
+      this.renderer.removeChild(editableDiv, placeholderElement);
     }
+  }
 
-    
-    /**
-     * Performs various actions based on the state of inputService properties.
-     * If nameType is 'EmojiType' or filename is not 'unset', it adds HTML tags.
-     * If nameType is 'unset' and setId is -1, it saves HTML tags and text.
-     * If 
-     *
-     * @returns {void}
-     */
-    async startApplicableButtonAction():Promise<void> {
-      if (this.inputService.nameType == 'EmojiType') this.addHTMLTags();
-      if (this.inputService.filename != 'unset') this.addHTMLTags();
-      if (
-        this.inputService.nameType == 'unset' 
-        && this.inputService.setId == -1
-        && !this.inputService.chatChange){
-        this.saveMessage();
-      } 
-      if (this.inputService.chatChange) {
-        this.removePlaceholder();
-        document.getElementById('inputDiv').innerText = '';
-        setTimeout(()=>{ this.restorePlaceholder();}, 500);
-        this.inputService.chatChange = false;
-      }
+
+  /**
+   * Restores the placeholder element if it does not exist.
+   *
+   * @returns {void}
+   */
+  restorePlaceholder(): void {
+    if (!document.querySelector('.placeholder')) {
+      const text = this.renderer.createText(this.inputService.placeholderText);
+      const newPlaceholder = this.renderer.createElement('div');
+      this.renderer.addClass(newPlaceholder, 'placeholder');
+      this.renderer.setAttribute(newPlaceholder, `id`, 'placholder');
+      this.renderer.appendChild(newPlaceholder, text);
+      this.renderer.setAttribute(newPlaceholder, 'contenteditable', 'true');
+      this.renderer.addClass(newPlaceholder, 'placeholder');
+      this.renderer.appendChild(this.editableDiv.nativeElement, newPlaceholder);
     }
+  }
 
 
-    /**
-     * Handles the selection of a user and generates HTML elements based on the user's data.
-     *
-     * @param {number} index - The index of the selected user in a data array.
-     * @returns {void}
-     */
-    selectedUser(index: number): void {
-      this.inputService.setId += 1;
-      this.inputService.textContent = '@' + this.dataService.userData[index].name;
-      this.inputService.emailContent = this.dataService.userData[index].email;
-      this.inputService.class = 'member';
-      this.inputService.linkTaget = 'unset';
-      this.inputService.name = this.dataService.userData[index].name;
-      this.inputService.filename = 'unset';
-      this.inputService.nameType = 'NameType';
-      this.inputService.userId = this.dataService.userData[index].userId;
-      this.addHTMLTags();
-      this.varService.sign = !this.varService.sign;
+  /**
+   * Handles the "onLeave" event, which checks if the inputDiv is empty and restores 
+   * the placeholder if necessary.
+   *
+   * @returns {void}
+   */
+  onLeave(): void {
+    let textLenght = document.getElementById('inputDiv').textContent.trim().length;
+    if (textLenght == 0) {
+      document.getElementById('inputDiv').innerText = '';
+      this.restorePlaceholder();
     }
+  }
+
+
+  /**
+   * Angular lifecycle hook that is called after the component has been initialized.
+   * Subscribes to the myVariable$ observable from inputService, and when a new value is emitted,
+   * it triggers the startApplicableButtonAction function if the new value is truthy.
+   *
+   * @returns {void}
+   */
+  ngOnInit(): void {
+    this.inputService.myVariable$.subscribe((newValue) => {
+      if (newValue) this.startApplicableButtonAction();
+    });
+  }
+
+
+  /**
+   * Executes actions based on the selected input type and button press.
+   *
+   * @returns {void}
+   */
+  startApplicableButtonAction(): void {
+    if (this.inputService.emojiSelected()) this.addHTMLTags();
+    if (this.inputService.fileUploadSelected()) this.addHTMLTags();
+    if (this.inputService.sendButtonPressed()) this.saveMessage();
+    if (this.inputService.newChatSelected()) this.resetInputField();
+  }
+
+
+  /**
+   * Reset the input field.
+   *
+   * @returns {void}
+   */
+  resetInputField(): void {
+    this.removePlaceholder();
+    document.getElementById('inputDiv').innerText = '';
+    setTimeout(() => { this.restorePlaceholder(); }, 500);
+    this.inputService.chatChange = false;
+  }
+
+
+  /**
+   * Handles the selection of a user and generates HTML elements based on the user's data.
+   *
+   * @param {number} index - The index of the selected user in a data array.
+   * @returns {void}
+   */
+  selectedUser(index: number): void {
+    this.inputService.setId += 1;
+    this.inputService.textContent = '@' + this.dataService.userData[index].name;
+    this.inputService.emailContent = this.dataService.userData[index].email;
+    this.inputService.class = 'member';
+    this.inputService.linkTaget = 'unset';
+    this.inputService.name = this.dataService.userData[index].name;
+    this.inputService.filename = 'unset';
+    this.inputService.nameType = 'NameType';
+    this.inputService.userId = this.dataService.userData[index].userId;
+    this.addHTMLTags();
+    this.varService.sign = !this.varService.sign;
+  }
 
 
   /**
@@ -142,44 +195,15 @@ export class MessageInputComponent {
     this.renderer.appendChild(inputDiv, ankerElement);
     let emptySpan = this.createEmptySpanElement();
     this.renderer.appendChild(inputDiv, emptySpan);
-    this.inputService.inputLinks[this.inputService.setId] = this.createLinkInfo();
+    this.inputService.inputLinks[this.inputService.setId] = this.inputService.createLinkInfo();
     this.setCursorWithClick(`${this.inputService.setId}` + 'Span');
   }
 
 
   /**
-   * Creates and returns a link information object based on properties from 'inputService'.
+   * Creates an anchor element (a) with specified properties and content.
    *
-   * @returns {object} - An object containing link information.
-   * @property {string} textContent - The text content of the link.
-   * @property {string} emailContent - The email content of the link.
-   * @property {string} class - The CSS class of the link.
-   * @property {string} linkTaget - The link target of the anchor element.
-   * @property {number} setId - The identifier for this link.
-   * @property {string} name - The name associated with the link.
-   * @property {string} filename - The filename associated with the link.
-   * @property {string} nameType - The type of name associated with the link.
-   * @property {string} userId - The user ID associated with the link.
-   */
-  createLinkInfo(): object {
-    return {
-      textContent: this.inputService.textContent,
-      emailContent: this.inputService.emailContent,
-      class: this.inputService.class,
-      linkTaget: this.inputService.linkTaget,
-      setId: this.inputService.setId,
-      name: this.inputService.name,
-      filename: this.inputService.filename,
-      nameType: this.inputService.nameType,
-      userId: this.inputService.userId,
-    };
-  }
-
-
-  /**
-   * Creates an HTML anchor element (<a>) with specific attributes and event listeners.
-   *
-   * @returns {HTMLElement} The newly created anchor element.
+   * @returns {HTMLElement} The created anchor element.
    */
   createAnkerElement(): HTMLElement {
     const ankerElement = this.renderer.createElement('a');
@@ -189,15 +213,24 @@ export class MessageInputComponent {
     this.renderer.setAttribute(ankerElement, 'contenteditable', 'false');
     this.renderer.setAttribute(ankerElement, `id`, `${this.inputService.setId}`);
     this.renderer.addClass(ankerElement, this.inputService.class);
-    if(this.inputService.nameType != 'EmojiType'){
-      this.renderer.listen(ankerElement, 'mouseenter', () => {
-        this.onSpanMouseEnter(`${index}`);
-      });
-      this.renderer.listen(ankerElement, 'mouseleave', () => {
-        this.onSpanMouseLeave(`${this.inputService.setId}`);
-      });
-    }
+    if (!this.inputService.emojiSelected()) this.addMouseEvent(ankerElement, index);
     return ankerElement;
+  }
+
+
+  /**
+   * Adds mouse event listeners to an anchor element.
+   *
+   * @param {HTMLElement} ankerElement - The anchor element to which listeners will be added.
+   * @returns {void}
+   */
+  addMouseEvent(ankerElement: HTMLElement, index: number): void {
+    this.renderer.listen(ankerElement, 'mouseenter', () => {
+      this.onSpanMouseEnter(`${index}`);
+    });
+    this.renderer.listen(ankerElement, 'mouseleave', () => {
+      this.onSpanMouseLeave(`${this.inputService.setId}`);
+    });
   }
 
 
@@ -216,9 +249,9 @@ export class MessageInputComponent {
 
 
   /**
-   * Extracts and saves HTML tags and text content from an editable HTML div.
+   * Extracts and saves HTML tags and text content from the editableDiv.
    *
-   * @returns {any[]} An array containing objects representing extracted HTML tags and text content.
+   * @returns {any[]} An array containing extracted HTML tags and text content.
    */
   saveHTMLTagsAndText(): any[] {
     this.inputService.contentArray = [];
@@ -226,16 +259,30 @@ export class MessageInputComponent {
     const children = editableDiv.childNodes;
     for (let i = 0; i < children.length; i++) {
       const child = children[i];
-      if (this.childIsAOrSpanTag(child)) {
-        let tagInfo = this.createTagInfoForAandSPANTags(child.tagName, i, child.innerHTML);
-        this.inputService.contentArray.push(this.addTagAttributes(child.attributes, tagInfo));
-      } else if (this.childIsText(child)) {
-        const textContent = child.textContent?.trim();
-        if (textContent) this.inputService.contentArray.push(this.createTextInfoForText(i, textContent));
+      this.fillContentArray(child, i);
+    }
+    this.inputService.addTagInfoLinkInfo();
+    return this.inputService.contentArray;
+  }
+
+
+  /**
+   * Fills the contentArray with information from the provided child element.
+   *
+   * @param {HTMLElement} child - The child element to extract information from.
+   * @param {number} i - The index of the child element.
+   * @returns {void}
+   */
+  fillContentArray(child: HTMLElement, i: number): void {
+    if (this.childIsAOrSpanTag(child)) {
+      let tagInfo = this.inputService.createTagInfoForAandSPANTags(child.tagName, i, child.innerHTML);
+      this.inputService.contentArray.push(this.inputService.addTagAttributes(child.attributes, tagInfo));
+    } else if (this.childIsText(child)) {
+      const textContent = child.textContent?.trim();
+      if (textContent) {
+        this.inputService.contentArray.push(this.inputService.createTextInfoForText(i, textContent));
       }
     }
-    this.addTagInfoLinkInfo();
-    return this.inputService.contentArray;
   }
 
 
@@ -262,74 +309,12 @@ export class MessageInputComponent {
 
 
   /**
-   * Creates a tag information object for 'A' and 'SPAN' HTML tags.
+   * Sets the cursor position to the start of the specified element and triggers a click event on it.
    *
-   * @param {string} tagType - The type of HTML tag ('A' or 'SPAN').
-   * @param {number} position - The position of the tag in the DOM.
-   * @param {string} content - The content of the tag.
-   * @returns {object} - A tag information object with properties.
-   */
-  createTagInfoForAandSPANTags(tagType: string, position: number, content: string): object {
-    return {
-      tagType: tagType,
-      position: position,
-      attributes: [{}],
-      content: content,
-      linkInfo: [{}],
-    };
-  }
-
-
-  /**
-   * Creates an information object for a text node in the content.
-   *
-   * @param {number} position - The position of the text node in the content.
-   * @param {string} content - The content of the text node.
-   * @returns {Object} An object containing information about the text node.
-   */
-  createTextInfoForText(position: number, content: string): object {
-    return {
-      tagType: 'text',
-      position: position,
-      attributes: [{}],
-      content: content,
-      linkInfo: [{}],
-    };
-  }
-
-
-  /**
-   * Adds attributes to a tag information object.
-   *
-   * @param {NamedNodeMap} attributes - The attributes of a DOM element.
-   * @param {any} tagInfo - The tag information object to which attributes will be added.
-   * @returns {Object} The updated tag information object with attributes.
-   */
-  addTagAttributes(attributes: NamedNodeMap, tagInfo: any): Object {
-    for (let j = 0; j < attributes.length; j++) {
-      const attribute = attributes[j];
-      let aInfo = { name: attribute.name, value: attribute.value };
-      tagInfo.attributes[j] = aInfo;
-    }
-    return tagInfo;
-  }
-
-
-  /**
-   * Adds link information to tag information objects based on attribute values.
-   *
+   * @param {string} id - The ID of the HTML element.
    * @returns {void}
    */
-  addTagInfoLinkInfo(): void {
-    this.inputService.contentArray.forEach(index => {
-      if (index.attributes.length > 3) {
-        index.linkInfo[0] = this.inputService.inputLinks[index.attributes[2].value];
-      }
-    });
-  }
-
-
-  setCursorWithClick(id:string): void {
+  setCursorWithClick(id: string): void {
     const el = document.getElementById(id);
     const range = document.createRange();
     const sel = window.getSelection();
@@ -338,65 +323,45 @@ export class MessageInputComponent {
     sel.removeAllRanges();
     sel.addRange(range);
     el.focus();
-    const doubleClickEvent = new MouseEvent('click', {
-      bubbles: true,
-      cancelable: true,
-      view: window,
-    });
+    const doubleClickEvent = new MouseEvent('click', {bubbles: true, cancelable: true,view: window});
     el.dispatchEvent(doubleClickEvent);
   }
 
 
-  saveMessage(){
+  /**
+   * Saves the message content, clears the input field, and restores the placeholder.
+   *
+   * @returns {void}
+   */
+  saveMessage(): void {
     this.directChatService.saveMessage2(this.saveHTMLTagsAndText());
     this.inputService.contentArray = [];
     document.getElementById('inputDiv').innerHTML = '';
-    setTimeout(()=>{ this.restorePlaceholder();}, 500);
+    setTimeout(() => { this.restorePlaceholder(); }, 500);
   }
 
 
   // Unused functions
-  deleteFile(i: number) {
+  /**
+   * Deletes a file associated with the input at the specified index and updates inputLink properties.
+   *
+   * @param {number} i - The index of the input link to be deleted.
+   * @returns {void}
+   */
+  deleteFile(i: number): void {
     const filepath = this.inputService.inputLinks[i].linkTaget;
     this.fileUploadService.deleteFile(filepath);
+    this.updatesInputLinkProperties(i);
+  }
+
+
+  /**
+   * updates inputLink properties.
+   * 
+   * @param {number} i - The index of the deleted input link. 
+   */
+  updatesInputLinkProperties(i: number): void {
     this.inputService.inputLinks[i].linkTaget = 'unset';
     this.inputService.inputLinks[i].filename = 'unset';
-  }
-
-  // Placeholder
-  ngAfterViewInit(){
-    this.restorePlaceholder();
-  }
-
-
-  removePlaceholder():void {
-    if (document.querySelector('.placeholder')) {
-    const editableDiv = this.editableDiv.nativeElement;
-    const placeholderElement = editableDiv.querySelector('.placeholder');
-    this.renderer.removeChild(editableDiv, placeholderElement);
-    }
-  }
-
-
-  restorePlaceholder() {
-    if(!document.querySelector('.placeholder')) {
-      const text = this.renderer.createText(this.inputService.placeholderText);
-      const newPlaceholder = this.renderer.createElement('div');
-      this.renderer.addClass(newPlaceholder, 'placeholder');
-      this.renderer.setAttribute(newPlaceholder, `id`, 'placholder');
-      this.renderer.appendChild(newPlaceholder, text);
-      this.renderer.setAttribute(newPlaceholder, 'contenteditable', 'true');
-      this.renderer.addClass(newPlaceholder, 'placeholder');
-      this.renderer.appendChild(this.editableDiv.nativeElement, newPlaceholder);
-    }
-  }
-
-
-  onLeave(){
-    let textLenght = document.getElementById('inputDiv').textContent.trim().length;
-    if (textLenght == 0){
-      document.getElementById('inputDiv').innerText = '';
-      this.restorePlaceholder();
-    }
   }
 }
--- src/app/message-input/service/message-input-service.service.ts
+++ src/app/message-input/service/message-input-service.service.ts
@@ -22,10 +22,10 @@ export class MessageInputServiceService {
   placeholderUserName: string = '';
   placeholderText: string = 'Nachicht an';
   chatChange: boolean = false;
-  
 
 
-  constructor() {}
+
+  constructor() { }
 
 
   /**
@@ -34,7 +34,7 @@ export class MessageInputServiceService {
    * @param {boolean} newValue - The new boolean value to set.
    * @returns {void}
    */
-  setMyVariable(newValue: boolean):void {
+  setMyVariable(newValue: boolean): void {
     this.myVariableSubject.next(newValue);
   }
 
@@ -64,7 +64,7 @@ export class MessageInputServiceService {
    *
    * @returns {void}
    */
-  insertFileLink():void {
+  insertFileLink(): void {
     this.setId += 1;
     this.emailContent = 'unset';
     this.class = 'member';
@@ -80,7 +80,7 @@ export class MessageInputServiceService {
    *
    * @returns {void}
    */
-  resetVariables():void {
+  resetVariables(): void {
     this.textContent = 'unset';
     this.emailContent = 'unset';
     this.class = 'unset';
@@ -99,30 +99,171 @@ export class MessageInputServiceService {
    * @param {string} href - The URL to open in a new tab.
    * @returns {void}
    */
-  openInNewTab(href: string){
+  openInNewTab(href: string) {
     console.log('openInNewTab', href);
     window.open(href, '_blank');
   }
 
 
-/**
- * Checks if a filename has a PDF file extension.
- *
- * @param {string} filename - The name of the file to be checked.
- * @returns {boolean} `true` if the file has a PDF extension, otherwise `false`.
- */
-fileIsPDF(filename: string): boolean {
-  let isPDFFile = false;
-  const filenameLength = filename.length;
-  if (
+  /**
+   * Checks if a filename has a PDF file extension.
+   *
+   * @param {string} filename - The name of the file to be checked.
+   * @returns {boolean} `true` if the file has a PDF extension, otherwise `false`.
+   */
+  fileIsPDF(filename: string): boolean {
+    let isPDFFile = false;
+    const filenameLength = filename.length;
+    if (
       filename[filenameLength - 1] === 'f' &&
       filename[filenameLength - 2] === 'd' &&
       filename[filenameLength - 3] === 'p' &&
       filename[filenameLength - 4] === '.'
-  ) {
+    ) {
       isPDFFile = true;
+    }
+
+    return isPDFFile;
   }
 
-  return isPDFFile;
-}
+
+
+  /**
+     * Check if the input type is 'EmojiType'.
+     *
+     * @returns {boolean} True if the input type is 'EmojiType', false otherwise.
+     */
+  emojiSelected(): boolean {
+    return this.nameType == 'EmojiType';
+  }
+
+
+  /**
+   * Check if a file is selected for upload.
+   *
+   * @returns {boolean} True if a file is selected, false otherwise.
+   */
+  fileUploadSelected(): boolean {
+    return this.filename != 'unset';
+  }
+
+
+  /**
+   * Check if the send button is pressed.
+   *
+   * @returns {boolean} True if the send button is pressed, false otherwise.
+   */
+  sendButtonPressed(): boolean {
+    return this.nameType == 'unset'
+      && this.setId == -1
+      && !this.chatChange;
+  }
+
+
+  /**
+   * Check if a new chat is selected.
+   *
+   * @returns {boolean} True if a new chat is selected, false otherwise.
+   */
+  newChatSelected(): boolean {
+    return this.chatChange;
+  }
+
+
+  /**
+     * Creates and returns a link information object based on properties from 'inputService'.
+     *
+     * @returns {object} - An object containing link information.
+     * @property {string} textContent - The text content of the link.
+     * @property {string} emailContent - The email content of the link.
+     * @property {string} class - The CSS class of the link.
+     * @property {string} linkTaget - The link target of the anchor element.
+     * @property {number} setId - The identifier for this link.
+     * @property {string} name - The name associated with the link.
+     * @property {string} filename - The filename associated with the link.
+     * @property {string} nameType - The type of name associated with the link.
+     * @property {string} userId - The user ID associated with the link.
+     */
+  createLinkInfo(): object {
+    return {
+      textContent: this.textContent,
+      emailContent: this.emailContent,
+      class: this.class,
+      linkTaget: this.linkTaget,
+      setId: this.setId,
+      name: this.name,
+      filename: this.filename,
+      nameType: this.nameType,
+      userId: this.userId,
+    };
+  }
+
+
+  /**
+   * Creates a tag information object for 'A' and 'SPAN' HTML tags.
+   *
+   * @param {string} tagType - The type of HTML tag ('A' or 'SPAN').
+   * @param {number} position - The position of the tag in the DOM.
+   * @param {string} content - The content of the tag.
+   * @returns {object} - A tag information object with properties.
+   */
+  createTagInfoForAandSPANTags(tagType: string, position: number, content: string): object {
+    return {
+      tagType: tagType,
+      position: position,
+      attributes: [{}],
+      content: content,
+      linkInfo: [{}],
+    };
+  }
+
+
+  /**
+   * Creates an information object for a text node in the content.
+   *  
+   * @param {number} position - The position of the text node in the content.
+   * @param {string} content - The content of the text node.
+   * @returns {Object} An object containing information about the text node.
+    */
+  createTextInfoForText(position: number, content: string): object {
+    return {
+      tagType: 'text',
+      position: position,
+      attributes: [{}],
+      content: content,
+      linkInfo: [{}],
+    };
+  }
+
+
+  /**
+   * Adds attributes to a tag information object.
+   *
+   * @param {NamedNodeMap} attributes - The attributes of a DOM element.
+   * @param {any} tagInfo - The tag information object to which attributes will be added.
+   * @returns {Object} The updated tag information object with attributes.
+   */
+  addTagAttributes(attributes: NamedNodeMap, tagInfo: any): Object {
+    for (let j = 0; j < attributes.length; j++) {
+      const attribute = attributes[j];
+      let aInfo = { name: attribute.name, value: attribute.value };
+      tagInfo.attributes[j] = aInfo;
+    }
+    return tagInfo;
+  }
+
+
+  /**
+   * Adds link information to tag information objects based on attribute values.
+   *
+   * @returns {void}
+   */
+  addTagInfoLinkInfo(): void {
+    this.contentArray.forEach(index => {
+      if (index.attributes.length > 3) {
+        index.linkInfo[0] = this.inputLinks[index.attributes[2].value];
+      }
+    });
+  }
+
 }
