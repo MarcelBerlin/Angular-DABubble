35fd8206cd2e8c584a87b78659bcf18d810a4e3c
HassSebastian <sebastian.hass76@gmail.com>
2023-10-06 19:37:53 +0200

    merge test

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@angular/core';
-import { Firestore, collectionData, collection, setDoc, doc, updateDoc, deleteDoc, addDoc, getDoc } from '@angular/fire/firestore';
+import {
+  Firestore,
+  collectionData,
+  collection,
+  setDoc,
+  doc,
+  updateDoc,
+  deleteDoc,
+  addDoc,
+  getDoc,
+} from '@angular/fire/firestore';
 import { User } from '../models/user.class';
 import { __param } from 'tslib';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
   users$: any = [];
@@ -18,36 +28,32 @@ export class DataService {
   forgotPasswordMenu: boolean = false;
   badgesArray: any[] = [];
 
-
-  constructor(
-    private firestore: Firestore,
-
-  ) {
+  constructor(private firestore: Firestore) {
     this.subcribeUserData();
   }
-
 
   subcribeUserData(): void {
     const coll = collection(this.firestore, 'users');
     this.users$ = collectionData(coll, { idField: 'id' });
     this.users$.subscribe((user: any) => {
       this.userData = user;
-      this.userData.sort((a, b) => { // von Basti eingefügte Sortierfunktion nach eingeloggtem User
+      this.userData.sort((a, b) => {
+        // von Basti eingefügte Sortierfunktion nach eingeloggtem User
         if (a.email === this.loggedInUserEmail) return -1;
         if (b.email === this.loggedInUserEmail) return 1;
         return a.email < b.email ? -1 : 1;
       });
-      if (this.loggedInUserData === undefined && localStorage.getItem('user')) this.getLoggedInUserData();
+      if (this.loggedInUserData === undefined && localStorage.getItem('user'))
+        this.getLoggedInUserData();
       if (this.loggedInUserData) this.updateUserDirectChatBagesAmount();
     });
   }
 
-
   /**
-  * Retrieves the logged-in user's data from the userData array and sets the necessary values.
-  * 
-  * @returns {void}
-  */
+   * Retrieves the logged-in user's data from the userData array and sets the necessary values.
+   *
+   * @returns {void}
+   */
   getLoggedInUserData(): void {
     this.userData.forEach((user: any) => {
       let userJson: any = localStorage.getItem('user');
@@ -59,10 +65,9 @@ export class DataService {
     });
   }
 
-
   /**
    * Retrieves the logged user's data from the provided user object.
-   * 
+   *
    * @param {object} user - The user object containing the logged user's data.
    * @returns {object} - An object containing the relevant user data.
    */
@@ -74,42 +79,40 @@ export class DataService {
       online: true,
       directChats: user.directChats,
       userId: user.id,
-      channels: user.channels
+      channels: user.channels,
     };
   }
 
-
   /**
    * Returns the logged user id.
-   * 
+   *
    * @returns {string} - The logged user id.
    */
   getUserID(): string {
     return this.loggedInUserData.userId;
   }
 
-
   /**
    * Save a new user doc to firestore, with the email of the signed up user email.
-   * 
+   *
    * @param {string} email - The email address of the signed up user.
    * @returns {void}
    */
-  saveSignUpUserData(email: string, name: string, img:string): void {
+  saveSignUpUserData(email: string, name: string, img: string): void {
     this.signUpUser.email = email;
     this.signUpUser.name = name;
-    this.signUpUser.img = img
+    this.signUpUser.img = img;
     const coll = collection(this.firestore, 'users');
-    setDoc(doc(coll), this.signUpUser.toJSON()).then(() => {
-    }).catch((error) => {
-      console.log('save user failed');
-    });
+    setDoc(doc(coll), this.signUpUser.toJSON())
+      .then(() => {})
+      .catch((error) => {
+        console.log('save user failed');
+      });
   }
-
 
   /**
    * Creates a new user with the provided user data, if logged in user not listed yet.
-   * 
+   *
    * @param {User} user - The user object containing the data to be saved.
    * @returns {void}
    */
@@ -117,40 +120,42 @@ export class DataService {
     this.signUpUser = user;
     const coll = collection(this.firestore, 'users');
     if (this.loggedInUserData == undefined) {
-      setDoc(doc(coll), this.signUpUser.toJSON()).then(() => {
-        console.log('google user created');
-      }).catch((error) => {
-        console.log('save user failed', error.code);
-      });
+      setDoc(doc(coll), this.signUpUser.toJSON())
+        .then(() => {
+          console.log('google user created');
+        })
+        .catch((error) => {
+          console.log('save user failed', error.code);
+        });
     }
   }
 
-
   /**
    * Updates the user data in Firestore.
-   * 
+   *
    * @returns {Promise<void>} A promise that resolves when the update operation is complete.
    */
   async updateUser(): Promise<void> {
     const qData = doc(this.firestore, 'users', this.loggedInUserData.userId);
     const newData = this.loggedInUserData;
-    updateDoc(qData, newData).then(() => {
-      // this.directChatActive = true;
-    }).catch((error) => {
-      console.log('update user failed');
-    })
+    updateDoc(qData, newData)
+      .then(() => {
+        // this.directChatActive = true;
+      })
+      .catch((error) => {
+        console.log('update user failed');
+      });
   }
-
 
   /**
    * Creates an array of badges representing the new message amounts for each user's direct chats.
-   * 
+   *
    * @returns {void}
    */
   createDirectChatBadges(): void {
     this.badgesArray = [];
     for (let i = 0; i < this.userData.length; i++) {
-      this.loggedInUserData.directChats.forEach(directChat => {
+      this.loggedInUserData.directChats.forEach((directChat) => {
         if (directChat.partnerId == this.userData[i].userId) {
           this.badgesArray[i] = directChat.newMessageAmount;
         } else if (this.badgesArray[i] == undefined) {
@@ -160,11 +165,10 @@ export class DataService {
     }
   }
 
-
   /**
    * Updates the new message amount for direct chats in the logged-in user's data
    * based on the stored data in local storage and then recreates the direct chat badges.
-   * 
+   *
    * @returns {void}
    */
   updateUserDirectChatBagesAmount(): void {
@@ -173,21 +177,11 @@ export class DataService {
       this.loggedInUserEmail = JSON.parse(userJson);
       if (user.email == this.loggedInUserEmail) {
         for (let i = 0; i < user.directChats.length; i++) {
-          this.loggedInUserData.directChats[i].newMessageAmount = user.directChats[i].newMessageAmount;
+          this.loggedInUserData.directChats[i].newMessageAmount =
+            user.directChats[i].newMessageAmount;
         }
       }
     });
     this.createDirectChatBadges();
   }
 }
-
-
-
-
-
-
-
-
-
-
-
