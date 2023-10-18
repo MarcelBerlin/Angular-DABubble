9311cdec8d6ebce592e96877e4d446a341c064c5
Stefan Boskamp <stboskamp@gmail.com>
2023-10-18 13:14:28 +0200

    resposiv work

--- src/app/dashboard/secondary-chat/secondary-chat.component.ts
+++ src/app/dashboard/secondary-chat/secondary-chat.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { VariablesService } from 'src/app/services/variables.service';
+import { DashboardComponentsShowHideService } from '../dashboard-components-show-hide.service';
 
 @Component({
   selector: 'app-secondary-chat',
@@ -7,6 +8,6 @@ import { VariablesService } from 'src/app/services/variables.service';
   styleUrls: ['./secondary-chat.component.scss']
 })
 export class SecondaryChatComponent {
- constructor(public varService: VariablesService){}
+ constructor(public varService: VariablesService, public dcshService: DashboardComponentsShowHideService){}
 
 }
